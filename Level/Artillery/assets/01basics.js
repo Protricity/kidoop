b0c936e09e6ebe8cbaf6a4cdbb1446f4b38832b9
ari <ari@asu.edu>
2015-04-26 19:37:18 -0700

    drag works

--- Level/Artillery/assets/01basics.js
+++ Level/Artillery/assets/01basics.js
@@ -16,68 +16,112 @@ var resume = function() {
     renderInterval = setInterval(doRender, 30)
 };
 
-var mouseDown = false;
-var lastAngle = 0;
+var lastPoint = null;
+var lastAngle = 20;
+var lastPower = 0.5;
+var isDragging = false;
 // var lastPower = 1;
 function onMouse(e) {
+    var dist = 0;
+    var distX = 0;
+    var distY = 0;
+    if(lastPoint) {
+        distX = e.pageX - lastPoint[0];
+        distY = e.pageY - lastPoint[1];
+        dist = Math.sqrt(distX*distX + distY*distY);
+    }
+    lastPoint = [e.pageX, e.pageY];
 
-    var eventName = null;
+    var tanks = document.getElementsByClassName('usertank');
+
     switch(e.type) {
         case 'click':
-            eventName = 'fire';
-            mouseDown = false;
+            isDragging = false;
             break;
 
         case 'mousedown':
-            mouseDown = true;
+            isDragging = true;
             return;
 
         case 'mouseup':
         case 'mousemove':
-            eventName = 'aim';
+            if(e.type === 'mouseup' && dist <= 5) {
+                for(var fi=0; fi<tanks.length; fi++) {
+                    var fireTank = tanks[fi];
+                    fireTank.dispatchEvent(new CustomEvent('fire', {
+                        detail: {
+                            angle: lastAngle,
+                            power: lastPower
+                        }
+                    }));
+                }
+                isDragging = false;
+                return;
+            }
+
+            if(isDragging) {
+
+                lastAngle = lastAngle - distY / 5  ;
+                if(lastAngle > 70)
+                    lastAngle = 70;
+                else if(lastAngle < 0 || lastAngle > 270)
+                    lastAngle = 0;
+
+                lastPower = lastPower + distX / 500;
+                if(lastPower > 1)
+                    lastPower = 1;
+                if(lastPower < 0.2)
+                    lastPower = 0.2;
+
+                for(var ai=0; ai<tanks.length; ai++) {
+                    var aimTank = tanks[ai];
+                    aimTank.dispatchEvent(new CustomEvent('aim', {
+                        detail: {
+                            angle: lastAngle,
+                            power: lastPower
+                        }
+                    }));
+                }
+
+                if(e.type === 'mouseup') {
+                    isDragging = null;
+                }
+            }
             break;
 
         default:
             throw new Error("Invalid Event: ", e);
     }
 
-    var tanks = document.getElementsByClassName('usertank');
 //     console.log(document.elementFromPoint(e.layerX, e.layerY));
-    for(var i=0; i<tanks.length; i++) {
-        var tank = tanks[i];
-        var bb = tank.getBoundingClientRect();
-        var dx = e.layerX - bb.left - bb.width/2;
-        var dy = e.layerY - bb.top - bb.height/2;
-        var d = Math.sqrt(dx*dx + dy*dy) - 100;
-        if(d<0) d=0;
-        var cannonAngle = (-50 + 360 + Math.atan2(dx, dy) * 120 / Math.PI) % 360;
-        if(cannonAngle<0 || cannonAngle>270) cannonAngle = 0;
-        if(cannonAngle>70) cannonAngle = 70;
+//    for(var i=0; i<tanks.length; i++) {
+//        var aimTank = tanks[i];
 
-        var cannonPower = (d<500?d:500)/500 + 0.2;
-        if(cannonPower > 1)
-            cannonPower = 1;
+        //aimTank.dispatchEvent(new CustomEvent('aim', {
+        //    detail: {
+        //        angle: lastAngle,
+        //        power: lastPower
+        //    }
+        //}));
 
-        if(lastAngle > cannonAngle)
-            lastAngle -= 0.5;
-        else if(lastAngle < cannonAngle) 
-            lastAngle += 0.5;
+        //var bb = aimTank.getBoundingClientRect();
+        //var dx = e.layerX - bb.left - bb.width/2;
+        //var dy = e.layerY - bb.top - bb.height/2;
+        //var d = Math.sqrt(dx*dx + dy*dy) - 100;
+        //if(d<0) d=0;
+        //var cannonAngle = (-50 + 360 + Math.atan2(dx, dy) * 120 / Math.PI) % 360;
+        //if(cannonAngle<0 || cannonAngle>270) cannonAngle = 0;
+        //if(cannonAngle>70) cannonAngle = 70;
+        //
+        //var cannonPower = (d<500?d:500)/500 + 0.2;
+        //if(cannonPower > 1)
+        //    cannonPower = 1;
+        //
 
-//         if(lastPower > cannonPower)
-//             lastPower -= 0.01;
-//         else if(lastPower < cannonPower)
-//             lastPower += 0.01;
-
-        tank.dispatchEvent(new CustomEvent(eventName, {
-            detail: {
-                angle: lastAngle,
-                power: cannonPower
-            }
-        }));
-    }
+    //}
 }
 
-document.addEventListener('click', onMouse, true);
+//document.addEventListener('click', onMouse, true);
 document.addEventListener('mousemove', onMouse, true);
 document.addEventListener('mouseup', onMouse, true);
 document.addEventListener('mousedown', onMouse, true);
